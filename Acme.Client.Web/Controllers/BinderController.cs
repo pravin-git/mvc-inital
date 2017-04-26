bf0e537087e5b6a75192e33415a4755731ce4221
pravin-git <pravinpawar@gmail.com>
2017-04-26 19:11:42 +0530

    Default Binders

---
+++ Acme.Client.Web/Controllers/BinderController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Acme.Common.DataContract;
+using Acme.Client.Web.Filters;
+
+namespace Acme.Client.Web.Controllers
+{
+    [LayoutInjector("_Layout")]
+    public class BinderController : BaseController
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            ICollection<UserRole> lst = new List<UserRole>()
+            {
+                new UserRole
+                {
+                    RoleId = 1,
+                    RoleName = "Sales"
+                },
+                new UserRole
+                {
+                    RoleId = 2,
+                    RoleName = "Admin"
+                },
+                new UserRole
+                {
+                    RoleId = 3,
+                    RoleName = "Supplier"
+                }
+            };
+            
+            return View(lst);
+        }
+
+        [HttpPost]
+        public ActionResult Index(ICollection<UserRole> lst)
+        {
+            return RedirectToAction("Index", "Home");
+        }
+
+    }
+}
